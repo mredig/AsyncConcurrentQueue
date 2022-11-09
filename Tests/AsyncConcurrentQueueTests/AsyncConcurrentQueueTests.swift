3c5be7f61eb7bb768f29446f12ffd1bab8312f05
Michael Redig <mredig@gmail.com>
2022-11-09 00:35:20 -0600

    (feat) concurrent queue test

--- Tests/AsyncConcurrentQueueTests/AsyncConcurrentQueueTests.swift
+++ Tests/AsyncConcurrentQueueTests/AsyncConcurrentQueueTests.swift
@@ -2,13 +2,11 @@ import XCTest
 @testable import AsyncConcurrentQueue
 
 final class AsyncConcurrentQueueTests: XCTestCase {
-
-    func testQueueItems() async throws {
+	func testQueueItems() async throws {
 		let queue = AsyncConcurrentQueue()
 
 		let counter = AtomicWrapper(value: [Int]())
 
-//		await queue.setMaximumConcurrentTasks(10)
 		let iterations = 20
 		for i in 1...iterations {
 			let task = await queue.queueTask {
@@ -31,6 +29,38 @@ final class AsyncConcurrentQueueTests: XCTestCase {
 		}
 
 		XCTAssertEqual(counter.value, counter.value.sorted())
+	}
+
+	func testConcurrentQueueItems() async throws {
+		let queue = AsyncConcurrentQueue()
+
+		let setCounter = AtomicWrapper(value: Set<Int>())
+
+		var mainSet: Set<Int> = []
+		let iterations = 100
+		await queue.setMaximumConcurrentTasks(iterations / 4)
+		for i in 1...iterations {
+			mainSet.insert(i)
+			let task = await queue.queueTask {
+				print("starting \(i)")
+				try await Task.sleep(nanoseconds: UInt64(Double.random(in: 0.01...0.1) * 1_000_000_000))
+				print("finishing \(i)")
+				return i
+			}
+
+			Task {
+				let value = try await task.value
+				var set = setCounter.value
+				set.insert(value)
+				setCounter.setValue(set)
+			}
+		}
+
+		while setCounter.value.count < iterations {
+			try await Task.sleep(nanoseconds: 1_000_000)
+		}
+
+		XCTAssertEqual(mainSet, setCounter.value)
 	}
 }
 

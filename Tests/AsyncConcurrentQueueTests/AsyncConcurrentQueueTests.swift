014500f82dc58109b22b1531f01a0d54ba4178aa
Michael Redig <mredig@gmail.com>
2022-11-09 00:43:15 -0600

    (feat) test queuing with cancellations

--- Tests/AsyncConcurrentQueueTests/AsyncConcurrentQueueTests.swift
+++ Tests/AsyncConcurrentQueueTests/AsyncConcurrentQueueTests.swift
@@ -38,7 +38,7 @@ final class AsyncConcurrentQueueTests: XCTestCase {
 
 		var mainSet: Set<Int> = []
 		let iterations = 100
-		await queue.setMaximumConcurrentTasks(iterations / 4)
+		await queue.setMaximumConcurrentTasks(iterations / 10)
 		for i in 1...iterations {
 			mainSet.insert(i)
 			let task = await queue.queueTask {
@@ -57,6 +57,49 @@ final class AsyncConcurrentQueueTests: XCTestCase {
 		}
 
 		while setCounter.value.count < iterations {
+			let current = await queue.currentlyExecutingTasks
+			let max = await queue.maximumConcurrentTasks
+			XCTAssertLessThanOrEqual(current, max)
+			try await Task.sleep(nanoseconds: 1_000_000)
+		}
+
+		XCTAssertEqual(mainSet, setCounter.value)
+	}
+
+	func testConcurrentQueueItemsWithCancellations() async throws {
+		let queue = AsyncConcurrentQueue()
+
+		let setCounter = AtomicWrapper(value: Set<Int>())
+
+		var mainSet: Set<Int> = []
+		let iterations = 100
+		await queue.setMaximumConcurrentTasks(iterations / 10)
+		for i in 1...iterations {
+			if i.isMultiple(of: 2) {
+				mainSet.insert(i)
+			}
+			let task = await queue.queueTask {
+				print("starting \(i)")
+				try await Task.sleep(nanoseconds: UInt64(Double.random(in: 0.01...0.1) * 1_000_000_000))
+				print("finishing \(i)")
+				return i
+			}
+
+			Task {
+				let value = try await task.value
+				var set = setCounter.value
+				set.insert(value)
+				setCounter.setValue(set)
+			}
+			if i.isMultiple(of: 2) == false {
+				task.cancel()
+			}
+		}
+
+		while setCounter.value.count < mainSet.count {
+			let current = await queue.currentlyExecutingTasks
+			let max = await queue.maximumConcurrentTasks
+			XCTAssertLessThanOrEqual(current, max)
 			try await Task.sleep(nanoseconds: 1_000_000)
 		}
 
